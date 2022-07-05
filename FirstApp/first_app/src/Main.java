1d471951ba4678d35583d7300f84b40a045a4faf
Lập trình cùng Eri <erichannel96@gmail.com>
2022-07-05 13:33:02 +0700

    update

--- FirstApp/first_app/src/Main.java
+++ FirstApp/first_app/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void test(String... eri) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
