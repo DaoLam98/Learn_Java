806b855509518bbde90c2376be3d3ee013554c05
Lập trình cùng Eri <erichannel96@gmail.com>
2022-07-25 18:06:42 +0700

    bcrypt

---
+++ Advance/encode_password/src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       String password = "123456789";
+       String encode = BCrypt.hashpw(password, BCrypt.gensalt(12));
+        System.out.println(encode);
+
+       /*
+       BCrypt.gensalt xác định số vòng, số vòng dao động từ 4-30,
+       số vòng càng lớn thì thời gian thực hiện băm càng lâu.
+        */
+
+        boolean valuate = BCrypt.checkpw(password, encode);
+        System.out.println(valuate);
+
+    }
+}
